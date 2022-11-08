90ae576ecbc6acdcc21781b831b9dbfc782fd7e8
tea-loves-jkdyd <aditi.6ti@gmail.com>
2022-11-08 14:24:34 +0530

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("stop making your life more difficult than it already is!(0-0)/ ");
+var canvas = new fabric.Canvas("canvas", {
+  width: 500,
+  height: 400,
+  backgroundColor: "#dcdcdc",
+  isDrawingMode: true,
+});
